396af34a4097a2205ebe2975936e6212e39ca156
Quinten <Q.verheij@hotmail.com>
2020-04-15 17:06:52 +0200

    Spring setup

---
+++ src/main/kotlin/com/projdgroep3/omgevingswet/OmgevingswetApplication.kt
@@ -0,0 +1,11 @@
+package com.projdgroep3.omgevingswet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OmgevingswetApplication
+
+fun main(args: Array<String>) {
+	runApplication<OmgevingswetApplication>(*args)
+}
---
+++ src/test/kotlin/com/projdgroep3/omgevingswet/OmgevingswetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.projdgroep3.omgevingswet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OmgevingswetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
